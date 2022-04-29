166f86f1e8e9262f6470418975843417ed5dc6b0
BeainaT <tania.df@hotmail.it>
2022-04-29 15:53:37 +0200

    added id on html + added js code

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 //show and hide hamburger menu on user click
+const showMenu = document.getElementById("open-menu");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+showMenu.addEventListener("click", 
+    function(){
+        hamburgerMenu.style.display = "block";
+    }
+);
+const hideMenu = document.getElementById("close-menu");
+hideMenu.addEventListener("click",
+    function(){
+        hamburgerMenu.style.display = "none";
+    }
+);
